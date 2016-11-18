5b7d7d7398af58f907c66ee8e5a69c88fd72f78c
Xander <xander.shandor@gmail.com>
2016-11-18 21:42:16 +0200

    ALU update

--- alu.vhd
+++ alu.vhd
@@ -5,34 +5,22 @@ ENTITY alu IS
 		OPERAND_A : IN BIT_VECTOR(N-1 DOWNTO 0);	
 		OPERAND_B : IN BIT_VECTOR(N-1 DOWNTO 0);
 		SEL_OP    : IN BIT_VECTOR(OP-1 DOWNTO 0);			
-		CLOCK     : IN BIT:= '1';
-		reset : IN BIT;
-		OUTPUT : OUT BIT_VECTOR(N-1 DOWNTO 0);
-		CARRYIN : IN BIT;
-		CARRYOUT : OUT BIT
+
+		OUTPUT : OUT BIT_VECTOR(N-1 DOWNTO 0)
 	);
 END alu;
 ARCHITECTURE behave_alu OF alu IS
 BEGIN
 
+PROCESS(OPERAND_A,OPERAND_B,SEL_OP)
 
-PROCESS(clock,sel_op)
-	VARIABLE temp_a : BIT_VECTOR(N-1 DOWNTO 0);
-	VARIABLE temp_b : BIT_VECTOR(N-1 DOWNTO 0);
-	VARIABLE temp_res: BIT_VECTOR(N-1 DOWNTO 0);
-	VARIABLE ovr : BIT;
-	BEGIN
-	IF clock = '1' AND clock'EVENT THEN
-		IF sel_op(0) ='0' AND sel_op(1)='0' THEN
-			temp_a:= OPERAND_A;
-			temp_b:= OPERAND_B;
-		ELSIF sel_op(0) ='0' AND sel_op(1)='1' THEN
-			--then add
-			temp_res:=suma(temp_a,temp_b,CARRYIN);
-		ELSIF sel_op(0) ='1' AND sel_op(1)='0' THEN
-			temp_res:=suma(temp_a,not temp_b,CARRYIN);
-		END IF;
-	END IF;
-	OUTPUT<= temp_res(N-1 DOWNTO 0);
-	END PROCESS;
+BEGIN
+	case sel_op(1) & sel_op(0) is
+		when "10"  =>
+			OUTPUT <= suma(OPERAND_A,NOT OPERAND_B,'1') after 1ns;
+		when "00" =>
+			OUTPUT <= suma(OPERAND_A,OPERAND_B,'0') after 1ns;
+		when others =>
+ 	END CASE;
+END PROCESS;
 END behave_alu;
