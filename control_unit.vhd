5919f5b1b64f17b8b339cdab252ed171bede81a4
Xander <xander.shandor@gmail.com>
2016-11-20 13:36:24 +0200

    Add substract

--- control_unit.vhd
+++ control_unit.vhd
@@ -1,38 +1,24 @@
+use work.sumpack.all;
 ENTITY control_unit IS
-	GENERIC(delay : TIME:=50ns); -- sincornizare pe front scazator -- r q a au clock  -aici se stabileste algoritmul
-	-- cum testatm ctrl unit
-	-- vezi pseudo cod pas 1 INBUS ...
-	-- fiecare e o stare
-
-	-- stare de test separat in care numa testez nu activeaza semnal de iesire
-	-- when case current_st is
-	--	=> s0 next state is ...
-	-- cand se afla intr-o stare se seteaza anumite iesiri 
-	-- declar a m q intern sa testez ca vectori de biti
-	-- pune delay la semnalele de shift clear etc....
-	
+	GENERIC(delay : TIME:=50ns; N: INTEGER:=4); -- sincornizare pe front scazator -- r q a au clock  -aici se stabileste algoritmul
 	PORT(
 		clock : IN BIT;
 		begins : IN BIT;
-		op : IN BIT_VECTOR(1 DOWNTO 0);
-		--m: IN BIT; -- assert loadA or not
-		--registrele functioneaza pe frontul crescator al clk 
-		ends : OUT BIT;
-		load_i: OUT BIT; -- incarca datele in registii operanzilor 
-		clear_a: OUT BIT; -- sterge continutul registrului a
-		load_a: OUT BIT; -- incarca continutul registrului a cu rezultatul adunarii
-		shift: OUT BIT; -- shifteaza continutul acumulatorului si a inmultitorului(coeficient)
-		add_sub: OUT BIT; -- select the operation
-		reset_f : OUT BIT -- reset the flip flop for q(-1)
-		
+		c : OUT BIT_VECTOR(6 DOWNTO 0);
+		--test out 
+		va : INOUT BIT_VECTOR(3 DOWNTO 0);
+
+		a_m:INOUT BIT_VECTOR(7 DOWNTO 0);
+		q: INOUT BIT_VECTOR(4 DOWNTO 0) --5 bits 1 of them for op
+
 	);
 END control_unit;
 
 ARCHITECTURE behave OF control_unit IS
 TYPE states IS (s0, s1, s2, s3);
 SIGNAL current_state, next_state : states:=s0;
-	SIGNAL  a,m,q: BIT_VECTOR(3 DOWNTO 0);
-	SIGNAL  c_0,c_1 : BIT;
+
+	SIGNAL  f_s : BIT; --witch together with q(0) gives us next state
 	CONSTANT ct_am : BIT_VECTOR(3 DOWNTO 0):="0000";
 	CONSTANT ct_q : BIT_VECTOR(3 DOWNTO 0):="0011";
 	CONSTANT ct_y : BIT_VECTOR(3 DOWNTO 0):="0101";
@@ -43,16 +29,58 @@ BEGIN
 		current_state <= next_state;
 	END IF;
 END PROCESS clock_process;
-state_transition_process:PROCESS(current_state,op)
+state_transition_process:PROCESS(current_state,q)
 	VARIABLE count :INTEGER:=0;
+	VARIABLE a,aux_a,aux_q:BIT_VECTOR(3 DOWNTO 0);
+	VARIABLE amshift:BIT_VECTOR(7 DOWNTO 0);
 BEGIN
 	CASE 	current_state IS 
 		WHEN s0 =>
-			a<=ct_am;
-			m<=ct_am;
-			q<=ct_y;
-			c_0<=q(0);
-			c_1<='0';
+			a:=ct_am;
+
+			a_m(7 downto 4)<=ct_am; -- initialize A register with 0000
+			a_m(3 downto 0)<=ct_am; -- initialize M register with 0000
+			--a_m<="00000001";
+			q(4 downto 1)<=ct_y;
+			q(0)<='0';
+
+			f_s<='0';
+			c<="0000001";
+			IF q(1)='1' AND q(0)='0'  THEN
+			 	next_state<= s1;
+			END IF;
+		WHEN s1 =>
+			--------------------------------------------------------
+			aux_a:=a_m(7 downto 4); --content of A
+			a:=suma(aux_a,NOT ct_y,'1');--sub from A value of c2's y
+			va<=a sra 1 after 100ns;
+			amshift(7 downto 4):=a; -- reconstruct content of A_M
+			amshift(3 downto 0):=a_m(3 downto 0);
+			amshift := amshift sra 1 ; -- right shift arithm
+
+			a_m <=amshift after 100ns; 	
+			--------------------------------------------------------
+			--q(0)<=q(1);
+			--aux_q:= q(4 downto 1) srl 1; 
+			--q(4 downto 1)<=aux_q;
+			--------------------------------------------------------
+			c<="0001001";
+			--IF q(1)='1' AND q(0)='1' THEN
+				next_state<= s2;
+			--END IF;
+		WHEN s2 =>
+			aux_a:=a_m(7 downto 4); --content of A
+			a:=suma(aux_a,ct_y,'0');--add from A value of c2's y
+			va<=a sra 1 after 100ns;
+			amshift(7 downto 4):=a; -- reconstruct content of A_M
+			amshift(3 downto 0):=a_m(3 downto 0);
+			amshift := amshift sra 1 ; -- right shift arithm
+
+			a_m <=amshift after 200ns; 
+			--c<="11001100";
+			IF q(1)='0' AND q(0)='1' THEN
+				next_state<= s1;
+			END IF;
 		WHEN others =>
 			-- do nothing
 	END CASE;
